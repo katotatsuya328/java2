66cf24ce195d0af5b6d94dce42407cf49f7ee6a2
katotatsuya <duelist.0810@docomo.ne.jp>
2023-03-20 22:52:32 +0900

    テスト

---
+++ Helloworld/src/sample/Sample.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package sample;
+
+/**
+ * @author dueli
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello,World");
+
+	}
+
+}
