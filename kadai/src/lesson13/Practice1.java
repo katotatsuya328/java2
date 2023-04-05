886c712a138ccb0b64a0b05a7ecb740014c1bf69
katotatsuya <duelist.0810@docomo.ne.jp>
2023-04-05 19:31:18 +0900

    カリキュラム課題1.13です。

---
+++ kadai/src/lesson13/Practice1.java
@@ -0,0 +1,30 @@
+package lesson13;
+
+
+
+/*
+int型の配列aを作成してください。
+配列aに1～5の数字を格納してください。
+繰り返し処理を使用し、
+配列aの数字を順番に表示させてください。
+
+出力結果：1
+		  2
+		  3
+		  4
+		  5
+*/
+
+public class Practice1 {
+
+public static void main(String[] args) {
+	// TODO 自動生成されたメソッド・スタブ
+	int [] a = {1,2,3,4,5};
+	//配列の中の実行数はa.lengthで宣言
+	for(int i = 0; i<a.length;i++) {
+		System.out.println(a[i]);
+	}
+
+}
+
+}
