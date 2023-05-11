f2ccbd68d2882112f0ada9397bbe19f113ad0fb9
katotatsuya <duelist.0810@docomo.ne.jp>
2023-05-11 22:33:31 +0900

    カリキュラム課題1.19です。

--- kadai/src/lesson19/Dog.java
+++ kadai/src/lesson19/Dog.java
@@ -1,16 +1,11 @@
 package lesson19;
 
 public class Dog {
-//	 Q1：フィールドに動物の名前の変数を定義してください。
+	//	 Q1：フィールドに動物の名前の変数を定義してください。
 		String name;
 
-//	 Q2：フィールドに動物の数の変数を定義してください。
+	//	 Q2：フィールドに動物の数の変数を定義してください。
 		int num;
-		
-//	public void AnimalNum(int x) {
-//		int num =5;
-//		System.out.println(x);	
-//	}
 		
 	//	Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。			
 	public Dog(){
